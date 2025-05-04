dd7c7d9165c14520d4a3bf27306faf95e338780f
Tetsuro Okubo <yo35shie@icloud.com>
2025-05-04 23:13:52 +0900

    コメント書いたり

--- src/Othello.java
+++ src/Othello.java
@@ -1,75 +1,83 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Othello {
     //盤面に関する情報
     private static final int SIZE = 8;
-    private static int[][] board = new int[SIZE][SIZE];
-    private static int[][] validMoves = new int[SIZE][SIZE];
-    private static final int EMPTY = 0;
-    private static final int BLACK = 1;
-    private static final int WHITE = 2;
-    private static final int CANPLACE = 3;
-    private static String turn = "Black";
+    private static int[][] board = new int[SIZE][SIZE]; //盤面
+    private static int[][] validMoves = new int[SIZE][SIZE]; //設置できる場所の盤面
+    private static final int EMPTY = 0; //設置されていない
+    private static final int BLACK = 1; //黒の石が置かれている
+    private static final int WHITE = 2; //白の石が置かれている
+    private static final int CANPLACE = 3;  //設置可能
+    private static String turn = "Black";   //手番（先手は黒）
 
-    //テスト用
+    // テスト用
     static Scanner scanner = new Scanner(System.in);
 
-
-    public Othello(){
-        initBoard();
-        System.out.println("Board is created.");
+    //コンストラクタ（そもそもインスタンスの生成は必要？？）
+    public Othello() {
+        initBoard();    //盤面の初期化
     }
 
-    //盤面初期化メソッド
-    public void initBoard() {
-        //全て石が置かれていない状態で初期化
-        for (int i = 0; i < SIZE; i++){
-            for(int j = 0;j < SIZE; j++){
+    // 盤面初期化メソッド
+    public static void initBoard() {
+        // 全て石が置かれていない状態で初期化
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 board[i][j] = EMPTY;
             }
         }
-            
-        //中心の4つのマスには白黒2つずつ予め配置
+
+        // 中心の4つのマスには白黒2つずつ予め配置
         board[3][3] = WHITE;
         board[3][4] = BLACK;
         board[4][3] = BLACK;
         board[4][4] = WHITE;
+
+        System.out.println("Board is initialised.");
     }
 
-    //盤面取得メソッド
-    public int[][] getBoard(){
+    // 盤面取得メソッド（表示させるわけではない）
+    public static int[][] getBoard() {
         return board;
     }
 
-    //手番取得メソッド
-    public String getTurn(){
+    // 手番情報取得メソッド
+    public static String getTurn() {
         return turn;
     }
 
-    static String opponent(String turn) {
-        return (turn == "Black") ? "White" : "Black";
+    //相手の手番情報取得メソッド
+    public static String opponent(String turn) {
+        if (turn == "Black") {
+            return "White";
+        } else {
+            return "Black";
+        }
     }
 
-    //手番交換メソッド
-    public void changeTurn(){
-        if(turn == "Black"){
+    // 手番交換メソッド
+    public static void changeTurn() {
+        if (turn == "Black") {
             turn = "White";
-        }else if(turn == "White"){
+        } else if (turn == "White") {
             turn = "Black";
         }
     }
 
-
-    //テスト用盤面表示メソッド
-    public void printBoard(int validMove[][]) {
+    // テスト用盤面表示メソッド
+    public static void printBoard(int validMove[][]) {
         System.out.print("  ");
-        for (int i = 0; i < SIZE; i++) System.out.print(i + " ");
+        for (int i = 0; i < SIZE; i++)
+            System.out.print(i + " ");
         System.out.println();
         for (int i = 0; i < SIZE; i++) {
             System.out.print(i + " ");
             for (int j = 0; j < SIZE; j++) {
-                if(board[i][j] == 0){
-                    if (validMove[i][j] == 3) {
+                if (board[i][j] == EMPTY) {
+                    if (validMove[i][j] == CANPLACE) {
                         System.out.print(validMove[i][j] + " ");
                         continue;
                     }
@@ -80,132 +88,171 @@ public class Othello {
         }
     }
 
-    //色に応じて石の設置可能場所を返すメソッド
+    // 色に応じて石の設置可能場所をboard形式で返すメソッド
     public int[][] getValidMoves(int[][] board, String turn) {
-        int[][] validMoves = new int[SIZE][SIZE];
+        int[][] validMoves = new int[SIZE][SIZE];   //returnする配列
 
+        //置けない場所は-1とする
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 validMoves[i][j] = -1;
             }
         }
 
+        //便宜上Stringの手番情報をintの色に変換
         int player = (turn == "Black") ? BLACK : WHITE;
         int opponent = (turn == "White") ? BLACK : WHITE;
-    
+
+        //設置場所について8方向においてそれぞれ確認するための8方向
         int[][] directions = {
-            {-1, -1}, {-1, 0}, {-1, 1},
-            { 0, -1},          { 0, 1},
-            { 1, -1}, { 1, 0}, { 1, 1}
+                { -1, -1 }, { -1, 0 }, { -1, 1 },
+                { 0, -1 },             { 0, 1 },
+                { 1, -1 },  { 1, 0 },  { 1, 1 }
         };
-    
+
+        //各マス目において
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
-                if (board[x][y] != 0) continue;  // 空きマスのみチェック
-    
+                // 既に石のあるマスはチェックする必要がない
+                if (board[x][y] != 0)
+                    continue;
+
+                //空きマスにおいて8方向それぞれを見ていく
                 for (int[] dir : directions) {
                     int i = x + dir[0], j = y + dir[1];
-                    boolean hasOpponentBetween = false;
-    
-                    // 相手の石を飛び越えて自分の石があるかを確認
+                    boolean hasOpponentBetween = false; //裏返せる石があるか（設置が可能なのか）
+
+                    // 相手の石を飛び越えて自分の石があるかを確認（設置可能条件を確認）
+                    //まず裏返す候補になりうる石があるかを確認
                     while (i >= 0 && i < 8 && j >= 0 && j < 8 && board[i][j] == opponent) {
                         i += dir[0];
                         j += dir[1];
                         hasOpponentBetween = true;
                     }
-    
+
+                    //その裏返す候補が本当に裏返せるか（自分の石で挟めるのか）を確認
                     if (hasOpponentBetween &&
-                        i >= 0 && i < 8 && j >= 0 && j < 8 &&
-                        board[i][j] == player) {
-                        validMoves[x][y] = CANPLACE;  // 設置可能
-                        break;  // 1方向でもOKなら次のマスへ
+                            i >= 0 && i < 8 && j >= 0 && j < 8 &&
+                            board[i][j] == player) {
+                        validMoves[x][y] = CANPLACE; // 設置可能
+                        break; // 1方向でもOKなら次のマスへ（どれくらい裏返せるのかは別メソッドで判断するからここでは置ければok）
                     }
                 }
             }
         }
 
-        return validMoves;
+        return validMoves; //設置可能場所をboard形式で返す
     }
 
-    //操作を反映し、石を裏返すメソッド
-    public void makeMove(int x, int y, String turn){
+    // 操作を反映し、石を裏返すメソッド
+    public void makeMove(int x, int y, String turn) {
 
-        if(turn == "Black"){
+        //まず置く場所に石を置く
+        if (turn == "Black") {
             board[x][y] = BLACK;
-        }else if(turn == "White"){
+        } else if (turn == "White") {
             board[x][y] = WHITE;
         }
 
+        //便宜上Stringの手番情報をintの色に変換
         int player = (turn == "Black") ? BLACK : WHITE;
         int opponent = (turn == "White") ? BLACK : WHITE;
-    
-        int[][] directions = {
-            {-1, -1}, {-1, 0}, {-1, 1},
-            { 0, -1},          { 0, 1},
-            { 1, -1}, { 1, 0}, { 1, 1}
-        };
-    
-        for (int[] dir : directions) {
-            int i = x + dir[0], j = y + dir[1];
 
-            // 相手の石を飛び越えて自分の石があるかを確認
+        //設置場所について8方向においてそれぞれ確認するための8方向
+        int[][] directions = {
+                { -1, -1 }, { -1, 0 }, { -1, 1 },
+                { 0, -1 },             { 0, 1 },
+                { 1, -1 },  { 1, 0 },  { 1, 1 }
+        };
+
+        //空きマスにおいて8方向それぞれを見ていく
+        for (int[] dir : directions) {
+            List<int[]> toFlip = new ArrayList<>();
+
+            int i = x + dir[0], j = y + dir[1];
+            boolean hasOpponentBetween = false; //裏返せる石があるか（設置が可能なのか）
+
+            // 相手の石を飛び越えて自分の石があるかを確認（設置可能条件を確認）
+            //まず裏返す候補になりうる石があるかを確認
             while (i >= 0 && i < 8 && j >= 0 && j < 8 && board[i][j] == opponent) {
-                board[i][j] = player;
+                toFlip.add(new int[]{i, j});
                 i += dir[0];
                 j += dir[1];
+                hasOpponentBetween = true;
             }
-        }
-    }
 
-    //ある色が自分の番において設置可能場所があるかどうかを返すメソッド
-    public boolean hasValidMoves(String turn){
-        int[][] validMoves = new int[SIZE][SIZE];
-        boolean isValidMove = false;
-
-        validMoves = getValidMoves(board, turn);
-
-        for(int x = 0; x < SIZE; x++){
-            for(int y = 0; y < SIZE; y++){
-                if(validMoves[x][y] == CANPLACE){
-                    isValidMove = true;
+            //その裏返す候補が本当に裏返せるか（自分の石で挟めるのか）を確認して、実際に裏返す
+            if (hasOpponentBetween && i >= 0 && i < 8 && j >= 0 && j < 8 && board[i][j] == player) {
+                for (int[] positionToFlip : toFlip) {
+                    board[positionToFlip[0]][positionToFlip[1]] = player;
                 }
             }
         }
-        
-        return isValidMove;
     }
 
-    //勝敗判定メソッド
-    public String judgeWinner() {
+    // ある色が自分の番において設置可能場所があるかどうかを返すメソッド
+    public boolean hasValidMoves(String turn) {
+
+        //全てのマスを走査し
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                //ある場所が設置可能であれば
+                if (getValidMoves(board, turn)[x][y] == CANPLACE) {
+                    return true;
+                }
+            }
+        }
+
+        //どのマスにも石が置けないときはこのfor文を抜け以下を返す
+        return false;
+    }
+
+
+    // こっちで設置可能場所を取得できるから相手の入力関係なく判断しても良いっちゃいい
+    // 自分がパスになったときに作用する、終了判定メソッド（置けないときはint[2] = {-1, -1}でいいんだっけ）
+    public boolean isFinished(int[] opponentMove) {
+        // 相手もパスだった時に終了するという考え
+        if (opponentMove[0] == -1 && opponentMove[1] == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 勝敗判定メソッド
+    public static String judgeWinner() {
+        //白黒それぞれの枚数をカウントしている
         int black = 0;
         int white = 0;
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
-                if(board[x][y] == BLACK){
+                if (board[x][y] == BLACK) {
                     black++;
                 }
 
-                if(board[x][y] == WHITE){
+                if (board[x][y] == WHITE) {
                     white++;
                 }
             }
         }
-        
-        //何も返さずにここで出力する可能性もある？？
-        if(black > white){
-            return "Black"; 
-        }else if(white > black){
+
+        //カウント結果から勝敗を判断する
+        // 何も返さずにここで出力する可能性もある？？
+        if (black > white) {
+            return "Black";
+        } else if (white > black) {
             return "White";
-        }else{
+        } else {
             return "Draw";
         }
-        
+
     }
 
-    public static void main(String[] args){
+    //テスト実行用
+    public static void main(String[] args) {
         Othello othello = new Othello();
 
-        othello.initBoard();
+        initBoard();
 
         turn = "Black";
 
@@ -213,7 +260,7 @@ public class Othello {
 
             validMoves = othello.getValidMoves(board, turn);
 
-            othello.printBoard(validMoves);
+            printBoard(validMoves);
             if (othello.hasValidMoves(turn)) {
                 System.out.println(turn + "の番です。x y の順に入力してください（0～7）:");
                 int x = scanner.nextInt();
@@ -223,22 +270,16 @@ public class Othello {
 
             } else {
                 System.out.println(turn + "はパスします。");
-                if (!(othello.hasValidMoves(opponent(turn)))) break;
+                if (!(othello.hasValidMoves(opponent(turn))))
+                    break;
             }
 
-            othello.changeTurn();
+            changeTurn();
         }
 
-        othello.printBoard(validMoves);
-        othello.judgeWinner();
+        printBoard(validMoves);
+        judgeWinner();
 
-        
     }
 
-}  
-
-
-//こっちで設置可能場所を取得できるから相手の入力関係なく判断しても良いやんけ
-
-
-    
+}
