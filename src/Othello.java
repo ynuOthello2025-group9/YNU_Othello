0380f82e9383d5265dc971b30e194c2fd67e3159
Tetsuro Okubo <yo35shie@icloud.com>
2025-05-06 13:39:46 +0900

    軽くリファクタイング

--- src/Othello.java
+++ src/Othello.java
@@ -94,6 +94,10 @@ public class Othello {
     //あるマスが設置可能かどうかを判定するメソッド
     public static boolean isValidMove(Integer[][] tempBoard, int x, int y, String turn){
 
+        if(tempBoard[x][y] != 0){
+            return false;
+        }
+
         // 便宜上Stringの手番情報をintの色に変換
         int player = (turn == "Black") ? BLACK : WHITE;
         int opponent = (turn == "White") ? BLACK : WHITE;
@@ -138,44 +142,11 @@ public class Othello {
             }
         }
 
-        // 便宜上Stringの手番情報をintの色に変換
-        int player = (turn == "Black") ? BLACK : WHITE;
-        int opponent = (turn == "White") ? BLACK : WHITE;
-
-        // 設置場所について8方向においてそれぞれ確認するための8方向
-        int[][] directions = {
-                { -1, -1 }, { -1, 0 }, { -1, 1 },
-                { 0, -1 }, { 0, 1 },
-                { 1, -1 }, { 1, 0 }, { 1, 1 }
-        };
-
         // 各マス目において
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
-                // 既に石のあるマスはチェックする必要がない
-                if (tempBoard[x][y] != 0)
-                    continue;
-
-                // 空きマスにおいて8方向それぞれを見ていく
-                for (int[] dir : directions) {
-                    int i = x + dir[0], j = y + dir[1];
-                    boolean hasOpponentBetween = false; // 裏返せる石があるか（設置が可能なのか）
-
-                    // 相手の石を飛び越えて自分の石があるかを確認（設置可能条件を確認）
-                    // まず裏返す候補になりうる石があるかを確認
-                    while (i >= 0 && i < 8 && j >= 0 && j < 8 && tempBoard[i][j] == opponent) {
-                        i += dir[0];
-                        j += dir[1];
-                        hasOpponentBetween = true;
-                    }
-
-                    // その裏返す候補が本当に裏返せるか（自分の石で挟めるのか）を確認
-                    if (hasOpponentBetween &&
-                            i >= 0 && i < 8 && j >= 0 && j < 8 &&
-                            tempBoard[i][j] == player) {
-                        validMoves[x][y] = CANPLACE; // 設置可能
-                        break; // 1方向でもOKなら次のマスへ（どれくらい裏返せるのかは別メソッドで判断するからここでは置ければok）
-                    }
+                if(isValidMove(tempBoard, x, y, turn)){
+                    validMoves[x][y] = CANPLACE; // 設置可能
                 }
             }
         }
@@ -188,44 +159,11 @@ public class Othello {
         
         List<int[]> validMoves = new ArrayList<>();
 
-        // 便宜上Stringの手番情報をintの色に変換
-        int player = (turn == "Black") ? BLACK : WHITE;
-        int opponent = (turn == "White") ? BLACK : WHITE;
-
-        // 設置場所について8方向においてそれぞれ確認するための8方向
-        int[][] directions = {
-                { -1, -1 }, { -1, 0 }, { -1, 1 },
-                { 0, -1 },             { 0, 1 },
-                { 1, -1 },  { 1, 0 },  { 1, 1 }
-        };
-
         // 各マス目において
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
-                // 既に石のあるマスはチェックする必要がない
-                if (tempBoard[x][y] != 0)
-                    continue;
-
-                // 空きマスにおいて8方向それぞれを見ていく
-                for (int[] dir : directions) {
-                    int i = x + dir[0], j = y + dir[1];
-                    boolean hasOpponentBetween = false; // 裏返せる石があるか（設置が可能なのか）
-
-                    // 相手の石を飛び越えて自分の石があるかを確認（設置可能条件を確認）
-                    // まず裏返す候補になりうる石があるかを確認
-                    while (i >= 0 && i < 8 && j >= 0 && j < 8 && tempBoard[i][j] == opponent) {
-                        i += dir[0];
-                        j += dir[1];
-                        hasOpponentBetween = true;
-                    }
-
-                    // その裏返す候補が本当に裏返せるか（自分の石で挟めるのか）を確認
-                    if (hasOpponentBetween &&
-                            i >= 0 && i < 8 && j >= 0 && j < 8 &&
-                            tempBoard[i][j] == player) {
-                            validMoves.add(new int[] { i, j });
-                        break; // 1方向でもOKなら次のマスへ（どれくらい裏返せるのかは別メソッドで判断するからここでは置ければok）
-                    }
+                if(isValidMove(tempBoard, x, y, turn)){
+                    validMoves.add(new int[] { x, y });
                 }
             }
         }
