4fb28dd82c111feb78db263b26d3dfe1f7f74098
augmented11th <misaqui0408@gmail.com>
2025-05-17 18:28:56 +0900

    置ける場所表示

--- src/View.java
+++ src/View.java
@@ -315,7 +315,7 @@ public class View extends JFrame implements ActionListener { // クラス名をV
         panel4_player.setLayout(new BoxLayout(panel4_player, BoxLayout.Y_AXIS));
         panel4_player.setBorder(BorderFactory.createEmptyBorder(10, 0, 30, 0));
 
-        gamescreen_label_playername = new JLabel("Player 1");
+        gamescreen_label_playername = new JLabel("Player");
         gamescreen_label_playername.setFont(new Font("MS Gothic", Font.PLAIN, 25));
         gamescreen_label_playername.setAlignmentX(Component.CENTER_ALIGNMENT);
         gamescreen_label_playerpiece = new JLabel("黒");
@@ -340,7 +340,7 @@ public class View extends JFrame implements ActionListener { // クラス名をV
         panel4_opp.setLayout(new BoxLayout(panel4_opp, BoxLayout.Y_AXIS));
         panel4_opp.setBorder(BorderFactory.createEmptyBorder(10, 0, 30, 0));
 
-        gamescreen_label_oppname = new JLabel("Player 2");
+        gamescreen_label_oppname = new JLabel("対戦相手を待っています");
         gamescreen_label_oppname.setFont(new Font("MS Gothic", Font.PLAIN, 25));
         gamescreen_label_oppname.setAlignmentX(Component.CENTER_ALIGNMENT);
         gamescreen_label_opppiece = new JLabel("黒");
@@ -612,12 +612,12 @@ public class View extends JFrame implements ActionListener { // クラス名をV
         cardLayout.show(cardPanel, MENU_SCREEN);
         setTitle("オセロゲーム"); // タイトルリセット
         // ゲーム画面の表示を初期状態に戻す (ScreenUpdaterのresetGameUIに相当)
-        if (gamescreen_label_playername != null) gamescreen_label_playername.setText("Player 1");
+        if (gamescreen_label_playername != null) gamescreen_label_playername.setText("Player");
         if (gamescreen_label_playerpiece != null) gamescreen_label_playerpiece.setText("黒");
-        if (gamescreen_label_playerpiececount != null) gamescreen_label_playerpiececount.setText("0枚");
-        if (gamescreen_label_oppname != null) gamescreen_label_oppname.setText("Player 1");
+        if (gamescreen_label_playerpiececount != null) gamescreen_label_playerpiececount.setText("2枚");
+        if (gamescreen_label_oppname != null) gamescreen_label_oppname.setText("対戦相手を待っています");
         if (gamescreen_label_opppiece != null) gamescreen_label_opppiece.setText("黒");
-        if (gamescreen_label_opppiececount != null) gamescreen_label_opppiececount.setText("0枚");
+        if (gamescreen_label_opppiececount != null) gamescreen_label_opppiececount.setText("2枚");
         if (gamescreen_label_turnplayer != null) gamescreen_label_turnplayer.setText("ゲーム待機中..."); // 初期表示に戻す
         if (gamescreen_button_surrender != null) gamescreen_button_surrender.setEnabled(false); // 退出ボタン無効化
         if (passButton != null) {
